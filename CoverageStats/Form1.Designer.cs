24525e9101725bb294073cbe49480779c5cd931c
iwan.szmigelski <ivan.shmihelskyy@thomsonreuters.com>
2018-02-22 10:37:48 +0100

    Adding button to test

--- CoverageStats/Form1.Designer.cs
+++ CoverageStats/Form1.Designer.cs
@@ -37,40 +37,43 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtColumnName = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // butonImportFile
             // 
             this.butonImportFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.butonImportFile.Location = new System.Drawing.Point(167, 36);
+            this.butonImportFile.Location = new System.Drawing.Point(263, 38);
             this.butonImportFile.Name = "butonImportFile";
-            this.butonImportFile.Size = new System.Drawing.Size(109, 44);
+            this.butonImportFile.Size = new System.Drawing.Size(126, 44);
             this.butonImportFile.TabIndex = 0;
-            this.butonImportFile.Text = "Import File";
+            this.butonImportFile.Text = "Coverage Stats";
             this.butonImportFile.UseVisualStyleBackColor = true;
             this.butonImportFile.Click += new System.EventHandler(this.buttonImportFile_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(167, 106);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 143);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(109, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(126, 126);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // txtResult
             // 
-            this.txtResult.Location = new System.Drawing.Point(47, 299);
+            this.txtResult.Location = new System.Drawing.Point(15, 275);
             this.txtResult.Multiline = true;
             this.txtResult.Name = "txtResult";
-            this.txtResult.Size = new System.Drawing.Size(362, 162);
+            this.txtResult.Size = new System.Drawing.Size(229, 162);
             this.txtResult.TabIndex = 3;
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(167, 501);
+            this.button2.Location = new System.Drawing.Point(262, 385);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(127, 52);
             this.button2.TabIndex = 4;
@@ -84,7 +87,8 @@
             this.txtSheetName.Name = "txtSheetName";
             this.txtSheetName.Size = new System.Drawing.Size(130, 20);
             this.txtSheetName.TabIndex = 5;
-            this.txtSheetName.Text = "Has Active Assets";
+            this.txtSheetName.Text = "Deals Report";
+            this.txtSheetName.TextChanged += new System.EventHandler(this.txtSheetName_TextChanged);
             // 
             // label1
             // 
@@ -106,26 +110,52 @@
             // 
             // txtColumnName
             // 
-            this.txtColumnName.Location = new System.Drawing.Point(11, 101);
+            this.txtColumnName.Location = new System.Drawing.Point(10, 101);
             this.txtColumnName.Name = "txtColumnName";
             this.txtColumnName.Size = new System.Drawing.Size(131, 20);
             this.txtColumnName.TabIndex = 8;
-            this.txtColumnName.Text = "issuer_orgid";
+            this.txtColumnName.Text = "DEAL_NO";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(382, 101);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(263, 101);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(126, 52);
             this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
+            this.button1.Text = "LEI Provider";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.runLEI_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(263, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 52);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "CA Import";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.runCA_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(263, 293);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 54);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "ImportSQLtoExcel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(505, 606);
+            this.ClientSize = new System.Drawing.Size(422, 449);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtColumnName);
             this.Controls.Add(this.label2);
@@ -136,7 +166,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.butonImportFile);
             this.Name = "Form1";
-            this.Text = "Coverage Stats";
+            this.Text = "ImportToSQL";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -154,6 +184,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtColumnName;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- CoverageStats/Form1.cs
+++ CoverageStats/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using CEMAutomationLib.Common;
 
 namespace CoverageStats
 {
@@ -19,12 +20,13 @@ namespace CoverageStats
 
         }
 
-        private void buttonImportFile_Click(object sender, EventArgs e)
+        public void buttonImportFile_Click(object sender, EventArgs e)
         {
             OpenFileDialog browse = new OpenFileDialog();
             browse.Multiselect = false;
             if (browse.ShowDialog() == DialogResult.OK)
             {
+                 
                 OleDbConnection conn = new OleDbConnection();
                 conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + browse.FileName + @";Extended Properties=""Excel 12.0 Xml;HDR=Yes;IMEX=1;ImportMixedTypes=Text;TypeGuessRows=0"""; //C:\Users\u0139221\Desktop\file.xlsx.
                 conn.Open();
@@ -55,7 +57,7 @@ namespace CoverageStats
                 }
                 string coverageList = Splitter.splitEndLineToInClauseOracle(result.ToString(), "o.org_nda_id",true);
                 string SQL = string.Format("select count(*)from oa.organizations o " +
-                 " where o.org_tier_id = 1 and {0} ", coverageList);
+                 " where o.org_tier_id = 1 and ({0}) ", coverageList);
 
                 txtResult.Text = SQL; //result.ToString();
             }
@@ -76,7 +78,113 @@ namespace CoverageStats
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void txtSheetName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public StringBuilder loadExcelColumn(string columnName, string sheetName)
+        {
+            OpenFileDialog browse = new OpenFileDialog();
+            browse.Multiselect = false;
+            if (browse.ShowDialog() == DialogResult.OK)
+            {
+                OleDbConnection conn = new OleDbConnection();
+                conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + browse.FileName + @";Extended Properties=""Excel 12.0 Xml;HDR=Yes;IMEX=1;ImportMixedTypes=Text;TypeGuessRows=0"""; //C:\Users\u0139221\Desktop\file.xlsx.
+                conn.Open();
+
+                DataTable a = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                OleDbCommand command = new OleDbCommand
+               (
+                    "SELECT " + columnName + "  FROM[" + sheetName + "$]", conn
+                );
+
+                DataTable dsnew = new DataTable();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+
+                adapter.Fill(dsnew);
+
+                dataGridView1.DataSource = dsnew;
+
+                StringBuilder result = new StringBuilder();
+
+                /* select count(*)from oa.organizations o
+                 where o.org_tier_id = 1 and (o.org_nda_id in ())*/
+
+                foreach (DataRow row in dsnew.Rows)
+                {
+                    result.Append(row[0]);
+                    result.Append("\n");
+                }
+
+                return result;
+                //string coverageList = Splitter.splitEndLineToInClauseOracle(result.ToString(), "o.orgid", true);
+                //string SQL = string.Format("select ORGID,ORG_DATA_PROVIDER from OA.ORGANIZATIONS o left join ife.entityids e on e.entityid = o.orgid " +
+                // " where orgid  in ({0}) ", coverageList);
+
+               // txtResult.Text = SQL; //result.ToString();
+            }
+            return new StringBuilder();
+        }
+
+
+        public void runLEI_Click(object sender, EventArgs e)
+        {
+            StringBuilder listRecords = loadExcelColumn(txtColumnName.Text, txtSheetName.Text);
+            StringBuilder result = new StringBuilder();
+            string coverageList = Splitter.splitEndLineToInClauseOracle(listRecords.ToString(), "ife.entityid", true);
+            //string SQL = string.Format("select ORGID,ORG_DATA_PROVIDER from OA.ORGANIZATIONS o left join ife.entityids e on e.entityid = o.orgid " +
+            // " where {0} ", coverageList);
+            string SQL = string.Format("select * from (select ife.entityid, ORG_DATA_PROVIDER, ad.USERname, ife.AUDITDATETIME " +
+            " from ife.audittrail ife left join OA.ORGANIZATIONS oa on ife.entityid = oa.orgid " +
+            " left join admin.users ad on ife.analystuserid = ad.userid where attributeid = 499 and newvalue like '%ID_TYPE>38</ID_TYPE%' " +
+             " and  {0}  order by ife.AUDITDATETIME DESC)" +
+            "where rownum = 1;" , coverageList);
+
+            txtResult.Text = SQL; //result.ToString();
+        }
+
+        private void runCA_Click(object sender, EventArgs e)
+        {
+
+            StringBuilder listRecords = loadExcelColumn(txtColumnName.Text, txtSheetName.Text);
+            StringBuilder result = new StringBuilder();
+            foreach (string record in listRecords.ToString().Split('\n'))
+            {
+                result.Append("'Deals No: ");
+                result.Append(record);
+                result.Append("',");
+            }         
+            string dealsList = result.ToString(0, result.Length - 1);
+            string SQL = string.Format("select req.REQUEST_ID, cc.COUNTRYdesc, req.ENTITY_ID, req.REQUEST_DATE, status.TASK_STATUS, req.ADDITIONAL_FIELD_VALUE_2, req.ANALYST_COMMENT " +
+                "from REQUESTTOOL.REQUEST_QUEUE req left join TASKTOOL.TASK_STATUS_TYPE_CODES status on req.status_id = status.TASK_STATUS_ID " +
+                "join REQUESTTOOL.request_detail reqd on reqd.request_id = req.request_id join domains.countrycodes cc on reqd.ATTRIBUTE_VALUE = cc.COUNTRYCODE and reqd.attribute_id = 259 " +
+                "where ADDITIONAL_FIELD_VALUE_2 in ({0}) and req.STATUS_ID in (1,2,3,4) and req.ADDITIONAL_FIELD_Name_1 = 'TARGET' AND cc.COUNTRYCODE not in ('ID','CA','BD', 'GB', 'AU', 'US', 'KR', 'SG', 'CN', 'HK', 'KY', 'BVI', 'BS', 'JP', 'IN', 'TW', 'VN', 'PH', 'MY','TH','NZ', 'IE', 'LK', 'ZA' )", dealsList);
+
+            txtResult.Text = SQL; //result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataBase db = new DataBase(DataBase.DATABASE_ORACLE);
+            bool testConn = db.testConnection();
+            DataSet result = db.getDataSet(txtResult.Text.ToString(), "Export");
+            ExcelManager excel = new ExcelManager();
+            excel.GenerateWorkbook(result);
+            excel.SaveIn("C:\\Users\\u0139221\\Desktop\\ImportSQl.xlsx");
+            excel.CleanAndClose();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
     }
 
